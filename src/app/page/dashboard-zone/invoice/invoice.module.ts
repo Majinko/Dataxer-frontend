73099589179a5ccca7b127db9588d28f2822da4c
Marek Hlavčo <marek@MacBook-Pro.local>
2020-09-07 20:02:25 +0200

    ll

--- src/app/page/dashboard-zone/invoice/invoice.module.ts
+++ src/app/page/dashboard-zone/invoice/invoice.module.ts
@@ -5,6 +5,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {InvoiceCreateComponent} from "./invoice-create/invoice-create.component";
 import {InvoiceIndexComponent} from "./invoice-index/invoice-index.component";
 import {InvoiceEditComponent} from "./invoice-edit/invoice-edit.component";
+import { InvoiceTableComponent } from './invoice-index/invoice-table/invoice-table.component';
 
 const routes: Routes = [{
   path: '',
@@ -26,7 +27,7 @@ const routes: Routes = [{
 }];
 
 @NgModule({
-  declarations: [InvoiceComponent],
+  declarations: [InvoiceComponent, InvoiceTableComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
