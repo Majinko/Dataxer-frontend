7a2a0d060e04fb59d9a86284b25b549909f29d9c
@marek <marek.hlavco@gmail.com>
2021-01-21 11:47:40 +0100

    detail ivnoice

---
+++ src/app/page/dashboard-zone/invoice/invoice-show/invoice-show.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {InvoiceService} from '../../../../core/services/invoice.service';
+import {Invoice} from '../../../../core/models/invoice';
+
+@Component({
+  selector: 'app-invoice-show',
+  templateUrl: './invoice-show.component.html',
+  styleUrls: ['./invoice-show.component.scss']
+})
+export class InvoiceShowComponent implements OnInit {
+  invoice: Invoice;
+
+  constructor(
+    private route: ActivatedRoute,
+    private invoiceService: InvoiceService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getById();
+  }
+
+  getById() {
+    this.invoiceService.getById(+this.route.snapshot.paramMap.get('id')).subscribe(invoice => {
+      this.invoice = invoice;
+    });
+  }
+}
