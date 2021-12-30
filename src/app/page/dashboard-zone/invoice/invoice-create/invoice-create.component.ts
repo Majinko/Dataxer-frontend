b112d5fd8da89f694af0bec7b25b892491229c27
marekhlavco <hlavco@dataid.sk>
2021-12-30 10:04:40 +0100

    upravy naklady

--- src/app/page/dashboard-zone/invoice/invoice-create/invoice-create.component.ts
+++ src/app/page/dashboard-zone/invoice/invoice-create/invoice-create.component.ts
@@ -5,7 +5,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {DocumentHelper} from '../../../../core/class/DocumentHelper';
 import {InvoiceService} from '../../../../core/services/invoice.service';
 import {UserService} from '../../../../core/services/user.service';
-import {addDays, APP_DATE_FORMATS, findInvalidControls} from '../../../../../helper';
+import {addDays, APP_DATE_FORMATS} from '../../../../../helper';
 import {NumberingService} from '../../../../core/services/numbering.service';
 import {CompanyService} from '../../../../core/services/company.service';
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
@@ -117,6 +117,7 @@ export class InvoiceCreateComponent implements OnInit {
             this.pathFromOldObject(invoice);
           });
         } else if (this.route.snapshot.paramMap.get('type') === 'SUMMARY_INVOICE') {
+          // tslint:disable-next-line:max-line-length
           this.invoiceService.summaryInvoice(+this.route.snapshot.paramMap.get('id'), this.route.snapshot.paramMap.get('fromType')).subscribe(invoice => {
             this.pathFromOldObject(invoice);
           });
