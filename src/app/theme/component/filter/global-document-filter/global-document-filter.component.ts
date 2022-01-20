a311f15f9d72a1ff59a2c66ad8f9a4495aba5d54
marekhlavco <hlavco@dataid.sk>
2022-01-20 19:33:04 +0100

    filter

--- src/app/theme/component/filter/global-document-filter/global-document-filter.component.ts
+++ src/app/theme/component/filter/global-document-filter/global-document-filter.component.ts
@@ -1,26 +1,27 @@
-import {Component, Injector, OnInit} from '@angular/core';
+import {Component, Injector, OnInit, Inject, Input} from '@angular/core';
 import {FilterClass} from '../../../../core/class/FilterClass';
 import {FormBuilder} from '@angular/forms';
 import {SearchBarService} from '../../../../core/services/search-bar.service';
 
 @Component({
   selector: 'app-global-document-filter',
-  templateUrl: './global-document-filter.component.html',
-  styleUrls: ['./global-document-filter.component.scss']
+  templateUrl: '../filter.component.html',
 })
 export class GlobalDocumentFilterComponent extends FilterClass implements OnInit {
+  @Input() modelName: string;
 
   constructor(
     public searchbarService: SearchBarService,
     public formBuilder: FormBuilder,
-    protected injector: Injector
+    protected injector: Injector,
+    @Inject('para') name: string
   ) {
     super(
       searchbarService,
       formBuilder,
-      'priceOffer',
+      name,
       ['title', 'contact.name'],
-      ['contact.id', 'company.id', 'project.id', 'month'],
+      ['contact.id', 'company.id', 'project.id', 'state', 'documentType', 'month'],
       injector
     );
   }
