46b56195cdd27c0ba1a904cc9025e6339b49bd31
Peter Malček <malcek@dataid.sk>
2022-02-28 14:31:45 +0100

    upd

--- src/app/page/dashboard-zone/task/task-detail/task-detail.component.ts
+++ src/app/page/dashboard-zone/task/task-detail/task-detail.component.ts
@@ -49,6 +49,18 @@ export class TaskDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.editorConfig.uploadUrl = '/test/image/upload';
+    this.editorConfig.toolbarHiddenButtons = [
+      [
+        'strikeThrough',
+        'heading',
+        'fontName',
+      ],
+      [
+        'customClasses',
+        'insertHorizontalRule',
+        'removeFormat',
+      ]
+    ];
 
     this.formGroup = this.formBuilder.group({
       comment: ['', Validators.required],
