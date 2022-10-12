367c37d3ab48c2cd392ad1fc876741a222fd9af1
Peter Malcek <malcek@dataid.sk>
2022-10-12 12:14:04 +0200

    upratanie

--- src/app/page/main-zone/dashboard-zone/cost/cost-create/cost-create.component.ts
+++ src/app/page/main-zone/dashboard-zone/cost/cost-create/cost-create.component.ts
@@ -156,8 +156,6 @@ export class CostCreateComponent extends DocumentHelperClass implements OnInit {
   }
 
   submit() {
-    console.log(this.formGroup.value);
-    return;
     this.submitted = true;
     this.isLoading = true;
 
@@ -175,7 +173,6 @@ export class CostCreateComponent extends DocumentHelperClass implements OnInit {
 
     if (this.f.project.value && !this.f.project.value?.id) {
       this.formGroup.get('isInternal').patchValue(true);
-      this.formGroup.get('project').patchValue(null, {emitEvent: false});
     }
 
     if (this.f.isRepeated.value === true) {
@@ -197,10 +194,6 @@ export class CostCreateComponent extends DocumentHelperClass implements OnInit {
         this.messageService.add('Náklad bol pridaný');
       });
     }, error => {
-      // todo zrjeme sa uz nepouziva
-      if (this.formGroup.get('isInternal').value) {
-        this.formGroup.get('project').patchValue('Firemný náklad');
-      }
       this.isLoading = false;
     });
   }
--- src/app/page/main-zone/dashboard-zone/cost/cost-edit/cost-edit.component.ts
+++ src/app/page/main-zone/dashboard-zone/cost/cost-edit/cost-edit.component.ts
@@ -160,9 +160,6 @@ export class CostEditComponent extends DocumentHelperClass implements OnInit {
       this.cost = cost;
 
       this.formGroup.patchValue(this.cost);
-      if (this.cost?.isInternal) {
-        this.formGroup.get('project').patchValue('Firemný náklad');
-      }
     });
   }
 
@@ -182,7 +179,6 @@ export class CostEditComponent extends DocumentHelperClass implements OnInit {
 
     if (this.f.project.value && !this.f.project.value?.id) {
       this.formGroup.get('isInternal').patchValue(true);
-      this.formGroup.get('project').patchValue(null, {emitEvent: false});
     }
 
     // set cost price and total price
@@ -190,11 +186,6 @@ export class CostEditComponent extends DocumentHelperClass implements OnInit {
       price: this.documentHelper.price,
       totalPrice: this.documentHelper.totalPrice,
     });
-
-    if (this.f.project.value && !this.f.project.value?.id) {
-      this.formGroup.get('isInternal').patchValue(true);
-      this.formGroup.get('project').patchValue(null, {emitEvent: false});
-    }
 
     this.costService.updateWithFiles(this.formGroup.value, this.uploadHelper.files).subscribe(() => {
       this.router.navigate(['/paginate/costs']).then(() => {
