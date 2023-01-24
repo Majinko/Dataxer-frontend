1312b48a757e14718e6981d74545914690214319
Peter Malcek <malcek@dataid.sk>
2023-01-24 15:59:19 +0100

    project security - clovekohodiny

--- src/app/page/main-zone/dashboard-zone/project/project-detail/components/project-user-evaluation-table/project-user-evaluation-table.component.ts
+++ src/app/page/main-zone/dashboard-zone/project/project-detail/components/project-user-evaluation-table/project-user-evaluation-table.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UserOverviewPrice} from '../../../../../../../core/models/user';
+import {
+  SecurityDialogComponent
+} from "../../../../../paginate-zone/components/security-dialog/security-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-project-user-evaluation-table',
@@ -7,15 +11,41 @@ import {UserOverviewPrice} from '../../../../../../../core/models/user';
   styleUrls: ['./project-user-evaluation-table.component.scss']
 })
 export class ProjectUserEvaluationTableComponent implements OnInit {
+  securityCode: string = '1234';
+  isSecurity = false;
 
   @Input() userOverviewPrice: UserOverviewPrice[] = [];
 
-  displayedColumns: string[] = ['name', 'hours', 'hourNetto', 'priceNetto', 'hourBrutto', 'priceBrutto'];
+  securityColumns: string[] = ['name', 'hours', 'hourNetto', 'priceNetto', 'hourBrutto', 'priceBrutto'];
+  defaultColumns: string[] = ['name', 'hours', 'hourBrutto', 'priceBrutto'];
+  displayedColumns: string[] = this.defaultColumns;
 
-  constructor() {
+  constructor(
+    protected dialog: MatDialog,
+  ) {
   }
 
   ngOnInit(): void {
+  }
+
+
+  security() {
+    if (!this.isSecurity) {
+      const dialogRef = this.dialog.open(SecurityDialogComponent, {
+        maxWidth: '400px',
+      });
+      dialogRef.afterClosed().subscribe(dialogResult => {
+        if (dialogResult) {
+          if (this.securityCode === dialogResult.code) {
+            this.isSecurity = true;
+            this.displayedColumns = this.securityColumns;
+          }
+        }
+      });
+    } else {
+      this.isSecurity = false;
+      this.displayedColumns = this.defaultColumns;
+    }
   }
 
 }
